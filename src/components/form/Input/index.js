c4a4c9f4e398f08d2977698eb6dae33f0fd88fcc
mwtz <martin.wiytiszen@rootstrap.com>
2022-04-11 12:18:48 -0300

    [feature]: signupPage's layout created and fields added

--- src/components/form/Input/index.js
+++ src/components/form/Input/index.js
@@ -2,9 +2,15 @@ import { string, func, object } from 'prop-types';
 import cn from 'classnames';
 import './styles.css';
 
-const Input = ({ register, type = 'text', name, error, handleFocus }) => (
+const Input = ({ register, type = 'text', name, error, handleFocus, placeholder = '' }) => (
   <div className="Input">
-    <input className={cn({ error })} type={type} {...register(name)} onFocus={handleFocus} />
+    <input
+      className={cn({ error })}
+      type={type}
+      {...register(name)}
+      onFocus={handleFocus}
+      placeholder={placeholder}
+    />
     <small className="error-message">{error?.message}</small>
   </div>
 );
---
+++ src/components/form/select/index.js
@@ -0,0 +1,27 @@
+import { string, func } from 'prop-types';
+import cn from 'classnames';
+import './styles.css';
+
+const Select = ({ register, options, name, error, placeholder, className = '', ...rest }) => (
+  <div className="Select">
+    <select {...register(name)} {...rest} className={cn(className)}>
+      <option value="" disabled selected className="placeholder">
+        {placeholder}
+      </option>
+      {options.map((value, i) => (
+        <option key={i} value={value}>
+          {value}
+        </option>
+      ))}
+    </select>
+    <small className="error-message">{error?.message}</small>
+  </div>
+);
+
+Select.propTypes = {
+  register: func.isRequired,
+  name: string,
+  type: string,
+};
+
+export default Select;
