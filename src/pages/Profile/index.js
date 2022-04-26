4609e62b1f0f05f7c33621019b7b32c95e73549f
mwtz <martin.wiytiszen@rootstrap.com>
2022-04-26 16:50:15 -0300

    [feature]: create a profile and new target pages

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import useTranslation from 'hooks/useTranslation';
+import { useLogoutMutation } from 'services/auth/auth';
+import useAuth from 'hooks/useAuth';
+import profile from 'assets/profile.svg';
+import smiles from 'assets/smilies.svg';
+import football from 'assets/football.svg';
+import travel from 'assets/travel.svg';
+import music from 'assets/music.svg';
+
+import './styles.scss';
+import { useHistory } from 'react-router';
+
+const Profile = () => {
+  const t = useTranslation();
+  const [logout, { isLoading }] = useLogoutMutation();
+
+  const {
+    user: { avatar, username },
+  } = useAuth();
+
+  const history = useHistory();
+  const handleLogout = () =>
+    logout()
+      .then(() => localStorage.removeItem('user'))
+      .then(() => {
+        history.push('/');
+      })
+      .catch(err => console.log(err));
+  return (
+    <div className="profile-container">
+      <h1 className="title">Target</h1>
+      <img src={avatar?.url || profile} alt="" className="avatar-img" />
+      <p className="username">{username}</p>
+      <div className="actions">
+        <a href="/profile" className="edit-profile">
+          Edit
+        </a>
+        <span className="slash">/</span>
+
+        <button className="logout" onClick={handleLogout} disabled={isLoading}>
+          {t('home.logoutBtn')}
+        </button>
+      </div>
+      <span className="separator" />
+      <h2 className="subtitle">Create your first target by clicking wherever on the map.</h2>
+      <p className="suggestion">Psss!, these are the most popular targets:</p>
+      <div className="list">
+        <div className="list-item">
+          <img src={football} alt="football bullet" className="list-bullet" />
+          <p className="list-text">Football</p>
+        </div>
+        <div className="list-item">
+          <img src={travel} alt="travel bullet" className="list-bullet" />
+          <p className="list-text">Travel</p>
+        </div>
+        <div className="list-item">
+          <img src={music} alt="music bullet" className="list-bullet" />
+          <p className="list-text">Music</p>
+        </div>
+      </div>
+      <img src={smiles} alt="smiles" className="smiles" />
+    </div>
+  );
+};
+
+export default Profile;
--- src/routes/routesPaths.js
+++ src/routes/routesPaths.js
@@ -1,7 +1,9 @@
 const routesPaths = {
   index: '/',
   login: '/login',
+  profile: '/profile',
   signup: '/sign-up',
+  newtarget: '/new-target',
 };
 
 export default routesPaths;
