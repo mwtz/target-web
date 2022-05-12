a7868e5720bf7d843739d007036c49079bcfd201
Rootstrap <rootstrap@Rootstraps-MacBook-Pro.local>
2022-05-11 21:19:09 -0300

    [feature]: chat page created

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import TopHeader from 'components/common/topheader';
+import useTranslation from 'hooks/useTranslation';
+import travel from 'assets/travel2.svg';
 import './styles.scss';
 
 const Chat = () => {
-  return <div className="chat-container">Chat</div>;
+  const t = useTranslation();
+
+  return (
+    <div className="conversation-container">
+      <TopHeader> {t('chatConversation.header')}</TopHeader>
+      <div className="chat-header">
+        <img src={travel} alt="" className="chat-topic" />
+        <div className="info">
+          <p className="user-name">{'Usuario Match'}</p>
+          <p className="topic-title">{'Target Title'}</p>
+        </div>
+        <div className="chat-box"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
